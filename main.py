35bae76465820f778e2bbf0b27e5b8f70ccc9949
Singh Abhinav D S <singhabhinavds@gmail.com>
2014-04-26 01:55:01 +0530

    Dynamic positioning of Buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import wx
+import wx.lib.sized_controls as sc
 
 
 class MainFrame(wx.Frame):
@@ -19,12 +20,21 @@ class MainFrame(wx.Frame):
         
         #Defining Position of each button
         ## Positions are absolute at the moment...will be made dynamic
-        button_view_data.SetPosition((15,15))
-        button_add_expenses.SetPosition((15,50))
-        button_review_expenses.SetPosition((15,100))
-        button_calculate_individual_balance.SetPosition((15,150))
-        button_exit_app.SetPosition((15,200))
+##        button_view_data.SetPosition((15,15))
+##        button_add_expenses.SetPosition((15,50))
+##        button_review_expenses.SetPosition((15,100))
+##        button_calculate_individual_balance.SetPosition((15,150))
+##        button_exit_app.SetPosition((15,200))
 
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(button_view_data, 1, wx.ALIGN_CENTER, 6)
+        sizer.Add(button_add_expenses, 1, wx.ALIGN_CENTER, 6)
+        sizer.Add(button_review_expenses, 1, wx.ALIGN_CENTER, 6)
+        sizer.Add(button_calculate_individual_balance, 1, wx.ALIGN_CENTER, 6)
+        sizer.Add(button_exit_app, 1, wx.ALIGN_CENTER, 6)
+        panel.SetSizer(sizer)
+
+        
         #Bind Events to buttons
 
         self.Bind(wx.EVT_BUTTON, self.OnExit, button_exit_app)
@@ -39,6 +49,7 @@ class MainFrame(wx.Frame):
 
 app = wx.App(False)
 frame = MainFrame(None, 'Hisaab')
-#Show the Main Frame
+#Show the Main Frame in center
+frame.CenterOnScreen()
 frame.Show(True)
 app.MainLoop()
