93da653063f78e1aa770d92e0b087fb827f56e94
Singh Abhinav D S <singhabhinavds@gmail.com>
2014-05-24 00:03:33 +0530

    Added sample sql statements

---
+++ SQL/budget_sheet_insert.sql
@@ -0,0 +1,4 @@
+insert into budget_sheet Values (3, 'beer', 50340, 2,'1,2','2014-03-20');
+insert into budget_sheet Values (4, 'bread', 512300, 1,'2','2014-03-21');
+insert into budget_sheet Values (5, 'shoes', 1500, 2,'1','2014-03-22');
+insert into budget_sheet Values (6, 'sample', 1000, 1,'1','2014-03-23');
---
+++ SQL/flatmates_insert.sql
@@ -0,0 +1,1 @@
+insert into flatmates(user_id,name) values (1, 'abhinav') , (2, 'ashish')
