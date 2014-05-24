b5e8f587ede0c9ad8747b61f730a7f0a9ea23d37
Singh Abhinav D S <singhabhinavds@gmail.com>
2014-05-25 00:55:47 +0530

    Displaying Data from database in budget sheet!
    dynamic rows are fixed. names are not displayed correctly instead user
    IDs are displayed.

--- classes/BudgetSheetGuiGrid.py
+++ classes/BudgetSheetGuiGrid.py
@@ -4,10 +4,10 @@ import mysql.connector
 
 class BudgetSheetGui(gridlib.PyGridTableBase):
 
-    def __init__(self, log, budget_rows_count):
-        print budget_rows_count
+    def __init__(self, log, DBQuery):
         gridlib.PyGridTableBase.__init__(self)
         self.log = log
+        self.DBQuery = DBQuery
 
         self.odd = gridlib.GridCellAttr()
         #self.odd.SetBackgroundColour('sky blue')
@@ -20,9 +20,8 @@ class BudgetSheetGui(gridlib.PyGridTableBase):
         attr.IncRef()
         return attr
 
-
     def GetNumberRows(self):
-        return 6
+        return self.DBQuery.budget_rows_count
 
     def GetNumberCols(self):
         return 7
@@ -31,8 +30,21 @@ class BudgetSheetGui(gridlib.PyGridTableBase):
         return False
 
     def GetValue(self, row, col):
-        #return str((row, col))
-        return 'lol'
+        if col == 0:
+            return self.DBQuery.store_rows_a[row]
+        elif col == 1:
+            return self.DBQuery.store_rows_b[row]
+        elif col == 2:
+            return self.DBQuery.store_rows_c[row]
+        elif col == 3:
+            return self.DBQuery.store_rows_d[row]
+        elif col == 4:
+            return self.DBQuery.store_rows_e[row]
+        elif col == 5:
+            return self.DBQuery.store_rows_f[row]
+        else:
+            #we are displaying on other columns
+            return ''
     
     def SetValue(self, row, col, value):
         self.log.write('SetValue(%d, %d, "%s") ignored.\n'
@@ -40,10 +52,10 @@ class BudgetSheetGui(gridlib.PyGridTableBase):
 
 class BudgetSheetGuiGrid(gridlib.Grid):
 
-    def __init__(self, parent, log, budget_rows_count):
+    def __init__(self, parent, log, DBQuery):
         gridlib.Grid.__init__(self, parent, -1)
 
-        table = BudgetSheetGui(log, budget_rows_count)
+        table = BudgetSheetGui(log, DBQuery)
         self.SetTable(table, True)
 
         self.Bind(gridlib.EVT_GRID_CELL_RIGHT_CLICK, self.OnRightDown)
@@ -58,7 +70,7 @@ class TestFrame(wx.Frame):
                           "Budget Sheet", size=(640, 480))
         
         DBQuery = BudgetSheetDBQuery(DB_NAME)
-        grid = BudgetSheetGuiGrid(self, log,DBQuery.budget_rows_count)
+        grid = BudgetSheetGuiGrid(self, log,DBQuery)
 
         #grid.SetReadOnly(5, 5, True)
 
@@ -69,17 +81,31 @@ class BudgetSheetDBQuery():
         cursor = connection.cursor()
         connection.database = dbname
 
+        self.store_rows_a = []
+        self.store_rows_b = []
+        self.store_rows_c = []
+        self.store_rows_d = []
+        self.store_rows_f = []
+        self.store_rows_e = []
+        
         query_flatmates = "Select * from flatmates"
         query_budget_sheet = "Select * from budget_sheet"
 
-        cursor.execute(query_flatmates)
-
-        for (fid, name) in cursor:
-            print "{}, {}".format(fid, name)
+##        cursor.execute(query_flatmates)
+##
+##        for (fid, name) in cursor:
+##            print "{}, {}".format(fid, name)
         
         cursor.execute(query_budget_sheet)
 
         for (a,b,c,d,e,f) in cursor:
-            print "{},{},{},{},{},{}".format(a,b,c,d,e,f)
+            self.store_rows_a.append(a)
+            self.store_rows_b.append(b)
+            self.store_rows_c.append(c)
+            self.store_rows_d.append(d)
+            self.store_rows_e.append(e)
+            self.store_rows_f.append(f)
+##            print "{},{},{},{},{},{}".format(a,b,c,d,e,f)
 
         self.budget_rows_count = cursor.rowcount
+        print cursor.rowcount
